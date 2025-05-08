65153365ae39920ba5411b1d4cc58b574e7d9295
LucianRoitman <49261770@est.ort.edu.ar>
2025-05-08 10:42:41 -0300

    Initial commit

---
+++ Assets/Scripts/NumeroMayor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumeroMayor : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 > num2)
+        {
+            Debug.Log("El mayor de los dos numeros ingresados es" + num1);
+        }
+        else if (num1 < num2)
+        {
+            Debug.Log("El mayor de los dos numeros ingresados es" + num2);
+        }
+        else if (num1 == num2)
+        {
+            Debug.Log("Los dos numeros enteros son iguales");
+        }
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/palabraSecreta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class palabraSecreta : MonoBehaviour
+{
+    string palabraCorrecta = "domingo";
+    public string palabra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (palabra != palabraCorrecta)
+        {
+            Debug.Log("Palabra incorrecta");
+            return;
+        }
+        else if (palabra == palabraCorrecta)
+        {
+            Debug.Log("Has adivinado la palabra");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
